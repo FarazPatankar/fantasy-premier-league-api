5046c78a7017ce0a0ee0acd7a0cb57b7b6813045
Faraz Patankar <farazpatankar@gmail.com>
2024-07-17 17:51:33 +0400

    fix prettier things

--- eslint.config.js
+++ eslint.config.js
@@ -4,71 +4,71 @@ import vitest from "eslint-plugin-vitest";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-	{
-		ignores: [
-			"coverage*",
-			"lib",
-			"node_modules",
-			"pnpm-lock.yaml",
-			"**/*.snap",
-		],
-	},
-	{
-		linterOptions: {
-			reportUnusedDisableDirectives: "error",
-		},
-	},
-	eslint.configs.recommended,
-	n.configs["flat/recommended"],
-	...tseslint.config({
-		extends: tseslint.configs.recommendedTypeChecked,
-		files: ["**/*.js", "**/*.ts"],
-		languageOptions: {
-			parserOptions: {
-				EXPERIMENTAL_useProjectService: {
-					allowDefaultProjectForFiles: ["./*.*s", "eslint.config.js"],
-					defaultProject: "./tsconfig.json",
-				},
-			},
-		},
-		rules: {
-			// These on-by-default rules don't work well for this repo and we like them off.
-			"no-constant-condition": "off",
+  {
+    ignores: [
+      "coverage*",
+      "lib",
+      "node_modules",
+      "pnpm-lock.yaml",
+      "**/*.snap",
+    ],
+  },
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: "error",
+    },
+  },
+  eslint.configs.recommended,
+  n.configs["flat/recommended"],
+  ...tseslint.config({
+    extends: tseslint.configs.recommendedTypeChecked,
+    files: ["**/*.js", "**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        EXPERIMENTAL_useProjectService: {
+          allowDefaultProjectForFiles: ["./*.*s", "eslint.config.js"],
+          defaultProject: "./tsconfig.json",
+        },
+      },
+    },
+    rules: {
+      // These on-by-default rules don't work well for this repo and we like them off.
+      "no-constant-condition": "off",
 
-			// These on-by-default rules work well for this repo if configured
-			"@typescript-eslint/no-unused-vars": ["error", { caughtErrors: "all" }],
-		},
-	}),
-	{
-		files: ["*.jsonc"],
-		rules: {
-			"jsonc/comma-dangle": "off",
-			"jsonc/no-comments": "off",
-			"jsonc/sort-keys": "error",
-		},
-	},
-	{
-		extends: [tseslint.configs.disableTypeChecked],
-		files: ["**/*.md/*.ts"],
-		rules: {
-			"n/no-missing-import": [
-				"error",
-				{ allowModules: ["fantasy-premier-league-api"] },
-			],
-		},
-	},
-	{
-		files: ["**/*.test.*"],
-		languageOptions: {
-			globals: vitest.environments.env.globals,
-		},
-		plugins: { vitest },
-		rules: {
-			...vitest.configs.recommended.rules,
+      // These on-by-default rules work well for this repo if configured
+      "@typescript-eslint/no-unused-vars": ["error", { caughtErrors: "all" }],
+    },
+  }),
+  {
+    files: ["*.jsonc"],
+    rules: {
+      "jsonc/comma-dangle": "off",
+      "jsonc/no-comments": "off",
+      "jsonc/sort-keys": "error",
+    },
+  },
+  {
+    extends: [tseslint.configs.disableTypeChecked],
+    files: ["**/*.md/*.ts"],
+    rules: {
+      "n/no-missing-import": [
+        "error",
+        { allowModules: ["fantasy-premier-league-api"] },
+      ],
+    },
+  },
+  {
+    files: ["**/*.test.*"],
+    languageOptions: {
+      globals: vitest.environments.env.globals,
+    },
+    plugins: { vitest },
+    rules: {
+      ...vitest.configs.recommended.rules,
 
-			// These on-by-default rules aren't useful in test files.
-			"@typescript-eslint/no-unsafe-assignment": "off",
-			"@typescript-eslint/no-unsafe-call": "off",
-		},
-	},
+      // These on-by-default rules aren't useful in test files.
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+    },
+  },
 );
